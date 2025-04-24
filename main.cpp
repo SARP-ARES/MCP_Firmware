35ac1d5c3fdc8bf942255cb8bf20cf67123de9b4
ollieb393 <62722139+ollieb393@users.noreply.github.com>
2025-04-23 22:45:50 -0700

    Minor Tweaks
    bro like some weird lines of code were removed idk why they were there

--- main.cpp
+++ main.cpp
@@ -15,10 +15,11 @@ int rightExtension = 0;
 // main() runs in its own thread in the OS
 int main()
 {
+    printf("main");
 
-    PID pid(0.017, 0, 1); // No idea if these values work
-    Motor motor(PA_8, PA_10, PB_2, PB_1, PB_15, PB_14, pid); // test bench
-    // Motor motor1(PB_3, PB_5, PA_11, PA_12, PA_10, PA_9, pid); // these are the mcpcb
+    ////PID pid(0.017, 0, 1); // No idea if these values work
+    ////Motor motor(PA_8, PA_10, PB_2, PB_1, PB_15, PB_14, pid); // test bench
+    //Motor motor1(PB_3, PB_5, PA_11, PA_12, PA_10, PA_9, pid); // these are the mcpcb
     //Motor motor2(PA_6, PA_5, PB_14, PB_15, PB_13, PA_8, pid);
 
 
@@ -44,11 +45,7 @@ int main()
         // go to position
         // motor1.lineTo(leftExtension, 10);
         // motor2.lineTo(rightExtension, 10);
-<<<<<<< Updated upstream
-        motor.lineTo(.5, 10);
-=======
+        ///// motor.lineTo(.5, 10);
         printf("test\n");
->>>>>>> Stashed changes
     }
 }
-
