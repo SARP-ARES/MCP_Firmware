452e3c284b5ae203225bd4d1166ef3f070c44185
ollieb393 <ollieb393@gmail.com>
2025-05-08 19:11:28 -0700

    Motor hangs

--- main.cpp
+++ main.cpp
@@ -16,33 +16,35 @@ int main()
 {
 
     PID pid(0.017, 0, 1); // No idea if these values work
-    Motor motor(PA_8, PA_10, PB_2, PB_1, PB_15, PB_14, pid); // test bench
-    Motor motor1(PB_3, PB_5, PA_11, PA_12, PA_10, PA_9, pid); // these are the mcpcb
-    Motor motor2(PA_6, PA_5, PB_14, PB_15, PB_13, PA_8, pid);
+    // Blackpill Motor config
+    Motor motor(PA_6, PA_7, PA_1, PA_2, PA_5, PA_3, &pid);
 
 
-    // on stm with breakout: PB_7 PB_8
-    I2CSerial ser(PB_7, PB_8, 0x32, true);
+    // // on stm with breakout: PB_7 PB_8
+    // I2CSerial ser(PB_7, PB_8, 0x32, true);
     
     
-    Distributor distributor(&ser);
-
+    // Distributor distributor(&ser);
+    
 
         //  DEBUG  //
     // printf("start :)\n");
-    // DigitalOut led1(PC_14);
+    DigitalOut led(PC_13);
 
     while (true) {
-        ThisThread::sleep_for(10ms);
-        // get distributed pair
-        std::pair<float, float> spoolExtensions = distributor.getMotorOutputs();
-        // if the first float is a NAN, keep each extension value the same
-        leftExtension = (isnan(spoolExtensions.first)) ? spoolExtensions.first : leftExtension;
-        rightExtension = (isnan(spoolExtensions.first)) ? spoolExtensions.second : rightExtension;
+        ThisThread::sleep_for(250ms);
+        led.write(1);
+        ThisThread::sleep_for(250ms);
+        led.write(0);
+        // // get distributed pair
+        // std::pair<float, float> spoolExtensions = distributor.getMotorOutputs();
+        // // if the first float is a NAN, keep each extension value the same
+        // leftExtension = (isnan(spoolExtensions.first)) ? spoolExtensions.first : leftExtension;
+        // rightExtension = (isnan(spoolExtensions.first)) ? spoolExtensions.second : rightExtension;
 
-        // go to position
-        motor1.lineTo(leftExtension, 10);
-        motor2.lineTo(rightExtension, 10);
+        // // go to position
+        // motor1.lineTo(leftExtension, 10);
+        // motor2.lineTo(rightExtension, 10);
     }
 }
 
