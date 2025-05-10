6b48ba0cab240f7cf8a968cef2599d9c521e47e6
ollieb393 <ollieb393@gmail.com>
2025-05-10 12:14:14 -0700

    Motor PWM control working

--- main.cpp
+++ main.cpp
@@ -3,24 +3,25 @@
 int main() {
     DigitalOut led(PC_13);
     DigitalOut led_ext(PB_0);
-    DigitalOut motor1(PA_0);
-    DigitalOut motor2(PA_1);
+    PwmOut motorPower(PA_1);
+    DigitalOut motorIn1(PA_6);
+    DigitalOut motorIn2(PA_5);
+
+    motorIn1.write(1);
+    motorIn2.write(0);
+    motorPower.write(0.5);
 
     bool m1 = true;
     bool m2 = false;
 
+    float power = 0;
     while (1) {
         // ThisThread::sleep_for(500ms);
         led = !led;
         led_ext = !led_ext;
-        motor1.write(0);
-        motor2.write(0);
-        ThisThread::sleep_for(50ms);
-        motor1.write(m1);
-        motor2.write(m2);
-        m1 = !m1;
-        m2 = !m2;
         ThisThread::sleep_for(2s);
-
+        power += 0.25;
+        if (power > 1) {power -= 1;}
+        motorPower.write(power);
     }
 }
