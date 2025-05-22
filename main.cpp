7f94572716a488839799d0e830a2e120c3ccd683
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2025-05-21 18:52:28 -0700

    cleaned up main - READY FOR FINAL TEST

--- main.cpp
+++ main.cpp
@@ -7,178 +7,66 @@
 
 
 
+void ctrl(float cmd, int seconds, MotorCOTS* motor1, MotorCOTS* motor2, Distributor* dstb, EUSBSerial* pc) {
+    Timer t;
+    t.start();
+    std::pair<float, float> extensions;
+    extensions = dstb->getMotorOutputsManual(cmd);
+    while (t.read_ms() < 1000*seconds) {
+        pc->printf("\tcmd: %f", cmd);
+        motor1->toPosition(extensions.first, 10);
+        motor2->toPosition(extensions.second, 10);
+        pc->printf("\n");
+        ThisThread::sleep_for(10ms);
+    }
+}
+
+void ctrl_manual(float cmd1, float cmd2, int seconds, MotorCOTS* motor1, MotorCOTS* motor2, EUSBSerial* pc) {
+    Timer t;
+    t.start();
+    while (t.read_ms() < 1000*seconds) {
+        pc->printf("cmd: full open");
+        motor1->toPosition(cmd1, 10);
+        motor2->toPosition(cmd2, 10);
+        pc->printf("\n");
+        ThisThread::sleep_for(10ms);
+    }
+}
+
+
+
 int main() {
-    DigitalOut led(PB_0);
-    led.write(1);
+    DigitalOut led(PC_13);
+    led.write(0);
     PID pid(1, 0, 5);
     EUSBSerial pc;
     ThisThread::sleep_for(1s);
     pc.printf("\nSerial Port Connected!\n");
 
     // direction 1, direction 2, throttle, encoder a, encoder b
-
-    // // ollie motor
     MotorCOTS motor1(PB_8, PB_9, PA_1, PA_6, PA_7, &pid, &pc);
     MotorCOTS motor2(PA_10, PA_9, PA_8, PA_15, PB_3, &pid, &pc);
-    led.write(0);
-    // jimmy motor:
-    // MotorCOTS motor(PA_2, PA_3, PB_1, PA_6, PA_7);
-
     float dt = 0.1;
-
     Distributor dstb;
-
-
-    // // not sure if we need this
-    // float degrees = 0;
-    // float current_angle = 0;
-    // float target_position = 0.1; // degrees
-    // pc.printf("Target Angle: %f\n\n", target_position);
-
-
     std::pair<float, float> extensions;
-    int seconds = 30;
 
 
-    //      CONTROL SEQUENCE
-    
-    Timer t;
-    t.start();
-
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("cmd: 0");
-        motor1.toPosition(0, 10);
-        motor2.toPosition(0, 10);
-        pc.printf("Loop 1\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    t.reset();
-    extensions = dstb.getMotorOutputsManual(1);
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("\tcmd: 1");
-        motor1.toPosition(extensions.first, 10);
-        motor2.toPosition(extensions.second, 10);
-        pc.printf("\tLoop 2\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    extensions = dstb.getMotorOutputsManual(0.5);
-    t.reset();
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("State: 0.5\t");
-        motor1.toPosition(extensions.first, 10);
-        motor2.toPosition(extensions.second, 10);
-        pc.printf("\tLoop 3\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    extensions = dstb.getMotorOutputsManual(0);
-    t.reset();
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("State: 0\t");
-        motor1.toPosition(extensions.first, 10);
-        motor2.toPosition(extensions.second, 10);
-        ThisThread::sleep_for(10ms);
-        pc.printf("\tLoop 4\n");
-    }
-
-    extensions = dstb.getMotorOutputsManual(-0.5);
-    t.reset();
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("State: -0.5\t");
-        motor1.toPosition(extensions.first, 10);
-        motor2.toPosition(extensions.second, 10);
-        pc.printf("\tLoop 5\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    extensions = dstb.getMotorOutputsManual(-1);
-    t.reset();
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("State: -1\t");
-        motor1.toPosition(extensions.first, 10);
-        motor2.toPosition(extensions.second, 10);
-        pc.printf("\tLoop 6\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    extensions = dstb.getMotorOutputsManual(1);
-    while (t.read_ms() < 1000*seconds) {
-        led = !led;
-        pc.printf("State: 1\t");
-        motor1.toPosition(extensions.first, 10);
-        motor2.toPosition(extensions.second, 10);
-        pc.printf("\tLoop 7\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    while (true) {
-        led = !led;
-        pc.printf("State: full open");
-        motor1.toPosition(0, 10);
-        motor2.toPosition(0, 10);
-        pc.printf("\tLoop 8 (END)\n");
-        ThisThread::sleep_for(10ms);
-    }
-
-    // ===================================
-
-    // while (t.read_ms() < 1000*seconds) {
-    //     led = !led;
-    //     pc.printf("State: full open");
-    //     motor1.toPosition(1, 10);
-    //     pc.printf("Loop 1\n");
-    //     ThisThread::sleep_for(10ms);
-    // }
-
-
-    // while (t.read_ms() < 1000*seconds) {
-    //     led = !led;
-    //     pc.printf("State: full open");
-    //     motor1.toPosition(0.5f, 10);
-    //     pc.printf("Loop 1\n");
-    //     ThisThread::sleep_for(10ms);
-    // }
-
-
-    // while (t.read_ms() < 1000*seconds) {
-    //     led = !led;
-    //     pc.printf("State: full open");
-    //     motor1.toPosition(0.2, 10);
-    //     pc.printf("Loop 1\n");
-    //     ThisThread::sleep_for(10ms);
-    // }
-
-
-    // while (t.read_ms() < 1000*seconds) {
-    //     led = !led;
-    //     pc.printf("State: full open");
-    //     motor1.toPosition(0, 10);
-    //     pc.printf("Loop 1\n");
-    //     ThisThread::sleep_for(10ms);
-    // }
-
-    // while (t.read_ms() < 1000*seconds) {
-    //     led = !led;
-    //     pc.printf("State: full open");
-    //     motor1.toPosition(1, 10);
-    //     pc.printf("Loop 1\n");
-    //     ThisThread::sleep_for(10ms);
-    // }
-
+    // tools:
+    // ctrl_manual(0, 0, 30, &motor1, &motor2, &pc); // cmd1, cmd2, duration (seconds)
     // while (true) {
-    //     led = !led;
-    //     pc.printf("State: full open");
-    //     motor1.toPosition(0, 10);
-    //     pc.printf("Loop 1\n");
-    //     ThisThread::sleep_for(10ms);
+    //     motor1.motorPower(-1);
+    //     // motor2.motorPower(1);
     // }
+    // ctrl_manual(1, 0, 300, &motor1, &motor2, &pc);
+
+
+    // CONTROL SEQUENCE:
+    // ctrl(cmd1, cmd2, ...)
+    ctrl(0.5, 60, &motor1, &motor2, &dstb, &pc);
+    ctrl(1, 120, &motor1, &motor2, &dstb, &pc);
+    ctrl(0, 60, &motor1, &motor2, &dstb, &pc);
+    ctrl(-0.5, 60, &motor1, &motor2, &dstb, &pc);
+    ctrl(-1, 60, &motor1, &motor2, &dstb, &pc);
+    ctrl(1, 999999999, &motor1, &motor2, &dstb, &pc);
 
 }
